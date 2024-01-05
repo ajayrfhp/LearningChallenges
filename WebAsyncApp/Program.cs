a295007f4b15abe97faf107e39a38a5069357659
ajayrfhp <ajayrfhp1710@gmail.com>
2024-01-05 02:56:57 -0800

    added web based async demo

---
+++ WebAsyncApp/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WebAsyncApp;
+
+Console.WriteLine("Hello, World!");
+
+double duration;
+DateTime start;
+DateTime end;
+
+Requests requests = new Requests();
+start = DateTime.Now;
+requests.DoSyncWork();
+end = DateTime.Now;
+duration = (end - start).TotalMilliseconds;
+Console.WriteLine("Sync work took {0} \n\n", duration);
+
+start = DateTime.Now;
+await requests.DoAsyncWork();
+end = DateTime.Now;
+duration = (end - start).TotalMilliseconds;
+Console.WriteLine("ASync work took {0}", duration);
+
+
---
+++ WebAsyncApp/Requests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAsyncApp
+{
+    public class Requests
+    {
+        public HttpClient HttpClient { get; set; }
+
+        public List<string> files { get; set; }
+
+        public Requests()
+        {
+            this.HttpClient = new HttpClient();
+            this.files = new List<string>
+            {
+                "https://filesamples.com/samples/document/txt/sample1.txt",
+                "https://filesamples.com/samples/document/txt/sample2.txt",
+                "https://filesamples.com/samples/document/txt/sample3.txt"
+            };
+        }
+
+        public void DoSyncWork()
+        {
+            foreach (string file in files)
+            {
+                Console.WriteLine("Begining Sync download for {0}", file);
+                this.HttpClient.GetAsync(file).Wait();
+                Console.WriteLine("Downloaded Sync data for {0}", file);
+            }
+        }
+
+        public async Task DoAsyncWork()
+        {
+            foreach (string file in files)
+            {
+                Console.WriteLine("Begining Async download for {0}", file);
+                await this.HttpClient.GetAsync(file);
+                Console.WriteLine("Downloaded Async data for {0}", file);
+            }
+        }
+
+    }
+}
